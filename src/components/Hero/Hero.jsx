e71630a15ece4bc3d105c51370b35867c3495eb1
Nakib64 <nafiz2282@gmail.com>
2025-07-01 02:13:45 +0600

    finished

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,8 +12,8 @@ const Hero = () => {
           className="w-60 md:w-100  object-cover rounded-full border-4 border-gray-200 shadow-lg"
         />
       </div>
-      <div className="text-center">
-        <h1 className="font-bold text-2xl md:text-6xl">👋 Hi! I'am</h1>
+      <div className="text-center md:text-left">
+        <h1 className="font-bold text-2xl md:text-6xl">Hi! I'am</h1>
         <h1 className="font-semibold text-xl md:text-5xl">Nafiz Uddowla Nakib,</h1>
         <h2 className="opacity-65 md:text-3xl"> a frontend-focused Mern-stack Developer</h2>
 
